7bca3d75c00344d1fa8369a2766824d53cce0832
Paras248 <paraspatil248@gmail.com>
2023-01-17 17:35:03 +0530

    Completed Day 1 task

---
+++ CharacterVowelOrConsonant.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CharacterVowelOrConsonant {
+    public static boolean isVowel(char ch){
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a character to check whether it is vowel or not: ");
+        char ch = sc.next().charAt(0);
+        System.out.println("--> The entered character is " + (isVowel(ch) ? "Vowel" : "Consonant"));        
+    }     
+}
